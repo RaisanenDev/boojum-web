f085c6c923cfa6fa83e236440ae75a23fc7c8dee
Mady R <raisanen.dev@gmail.com>
2019-07-24 15:58:36 -0400

    Beginprocessfix
    Began fixing the processor. Not ready just yet.

--- maps/api.js
+++ maps/api.js
@@ -86,23 +86,45 @@ function initMap() {
 
 }
 
+//needs to kind of look like this, but with my data
+
+//48.2439|
+//-71.0748|
+//<a href=http://sirsiweb.laurentian.ca/uhtbin/cgisirsi/x/x/x/57/5?user_id=WEBSERVER&library=DESMARAIS&searchdata1=Chicoutimi,%20Que.%20Mine%20Site{830}>Chicoutimi, Que. Mine Site</a>
+//<br>(Chicoutimi, Que.)<br>
+//<a href=http://sirsiweb.laurentian.ca/uhtbin/cgisirsi/x/x/x/57/5?user_id=WEBSERVER&library=DESMARAIS&searchdata1=ALCAN{830}>ALCAN</a>
+//<br>1985|
+//ALCAN, Chicoutimi, Que. Mine Site
+
 process_it_geo = function (doc) {
   // === split the document into lines ===
 
 
   lines = doc.split("\n");
-  for (var i = 0; i < lines.length; i++) {
+  for (var i = 0; i < jsonresponse.length-1; i++) {
+    jsonresponse[i].title
+    jsonresponse[i].coverage
+    jsonresponse[i].date
+    jsonresponse[i].identifier
+
+
     if (lines[i].length > 1) {
       // === split each line into parts separated by "|" and use the contents ===
-      parts = lines[i].split("|");
+
+      //This need to be edited to regex search for geo data, and then maybe put it into a single variable
       var lat = parseFloat(parts[0]);
       var lng = parseFloat(parts[1]);
-      var html = parts[2];
+
+      //This needs to parse out the identifier, generate a good name for the report, and give the location (Maybe instead of giving location, make it give the year... dunno)
+      var html = `<a href=${jsonresponse[i].identifier}>${jsonresponse[i].title[0]} - ${jsonresponse[i].coverage[1]}</a>`;
+      //This is the title of the paper, but originally it looks like the site name
       var label = parts[3];
+      //See this is what I meant for geo data, instead of making two variables just automatically put it into the point object
       var point = {
         lat: lat,
         lng: lng
       };
+      //Then create marker as normal
       // create the marker
       var marker = createMarker(point, label, html);
     }
@@ -110,6 +132,8 @@ process_it_geo = function (doc) {
   // put the assembled side_bar_html contents into the side_bar div
   document.getElementById("side_bar").innerHTML = side_bar_html;
 }
+
+//Now need to think about what to do with non geo data. Have a seperate sidebar maybe?
 
 
 function currentIndex() {
